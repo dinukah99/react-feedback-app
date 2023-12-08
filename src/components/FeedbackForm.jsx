4aee9b5deda781bf172dbbd148c6fc6016c1a016
dinukah99 <dinukahamarasinghe@gmail.com>
2023-12-08 18:42:49 +0530

    Real time validation for input field

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,11 +2,22 @@ import Card from './shared/Card';
 import {useState} from 'react';
 import Button from './shared/Button';
 
-
 const FeedbackForm = () => {
     const [text, setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState('');
 
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true);
+            setMessage(null);
+        } else if (text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true);
+            setMessage('Text must be at least 10 character');
+        } else {
+            setBtnDisabled(false);
+            setMessage(null);
+        }
         setText(e.target.value);
     };
 
@@ -17,8 +28,9 @@ const FeedbackForm = () => {
                 {/* @TODO - rating select component */}
                 <div className="input-group">
                     <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-                    <Button type="submit">Send</Button>
+                    <Button type="submit" isDisabled={btnDisabled}>Send</Button>
                 </div>
+                {message && <div className="message">{message}</div>}
             </form>
         </Card>
     );
